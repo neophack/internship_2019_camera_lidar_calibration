ed54e1f29286ceb20988f3805adc7e626308fb05
Tatiana <45382631+Tatfi32@users.noreply.github.com>
2020-01-20 20:01:44 +0300

    Add files via upload

---
+++ __init__.py
@@ -0,0 +1,14 @@
+from pathlib import Path
+
+from pcap_reader import *
+from yaml_reader import *
+from logger import *
+from smi_calculation import *
+
+
+if __name__ == '__main__':
+
+    #local_yml_reader = YML_Read(Path().absolute())
+    #local_yml_reader.power()
+    local_calibrator = SMI_calculations(Path().absolute()/ 'results')
+    local_calibrator.power()

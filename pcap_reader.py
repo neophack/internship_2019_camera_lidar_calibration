473f5e814b451dcb13993c38f6f3c5507111e9b0
vicproon <vicproon@gmail.com>
2020-01-23 15:26:32 +0300

    [upd] increased pcap read speed (by 10x) by replacing usage of intermediate DataFrames with lists of dicts. [fix] cross-platform path processing (now the same code works with windows and linux)

--- pcap_reader.py
+++ pcap_reader.py
@@ -6,6 +6,16 @@ import pandas as pd
 
 import time
 
+import progressbar #pip install progressbar2
+
+class Timer:    
+    def __enter__(self):
+        self.start = time.clock()
+        return self
+
+    def __exit__(self, *args):
+        self.end = time.clock()
+        self.interval = self.end - self.start
 
 class PointReader:
     def __init__(self, data_path):
@@ -19,15 +29,17 @@ class PointReader:
 
         self.df = pd.DataFrame({'X': [], 'Y': [], 'Z': [], 'D': [], 'azimuth': [], 'laser_id': [],
                                 'first_timestamp': [], 'pcap_num': []})
-        self.local_df = pd.DataFrame({'X': [], 'Y': [], 'Z': [], 'D': [], 'azimuth': [], 'laser_id': [],
-                                      'first_timestamp': [], 'pcap_num': []})
+        self.local_df = []
 
         self.azimuth_bin = 100
         self.first_timestamp = None
         self.factory = None
 
     def read_pcap(self, file_number):
-        self.get_pcap_data(file_number)
+        with Timer() as t:
+            self.get_pcap_data(file_number)
+        print('PointReader.read_pcap finished in %.03f s' % t.interval)
+
         return self.df
 
     def get_pcap_data(self, file_number):
@@ -36,28 +48,29 @@ class PointReader:
         pcap_data = open(pcap_file, 'rb').read()
         pcap_data = pcap_data[24:]
 
-        for offset in range(0, int(len(pcap_data)), 1264):
-            counter = offset // 1264
-            if counter % 30 == 0:
-                print("offset=", offset, int(len(pcap_data)))
+        with progressbar.ProgressBar(max_value=int(len(pcap_data))) as bar:
+            for offset in range(0, int(len(pcap_data)), 1264):
+                counter = offset // 1264
+                bar.update(offset)
+                # if counter % 30 == 0:
+                #    print("offset=", offset, int(len(pcap_data)))
+                    # if counter > 1:
+                    #     return
 
-                if self.local_df.empty is False:
-                    self.df = self.df.append(self.local_df)
-                    self.local_df = pd.DataFrame({'X': [], 'Y': [], 'Z': [], 'D': [], 'azimuth': [], 'laser_id': [],
-                                 'first_timestamp': [], 'pcap_num': []})
+                if (len(pcap_data) - offset) < 1264:
+                    break
 
-            if (len(pcap_data) - offset) < 1264:
-                break
+                cur_packet = pcap_data[offset + 16: offset + 16 + 42 + 1200 + 4 + 2]
+                cur_data = cur_packet[42:]
 
-            cur_packet = pcap_data[offset + 16: offset + 16 + 42 + 1200 + 4 + 2]
-            cur_data = cur_packet[42:]
+                self.first_timestamp, self.factory = struct.unpack_from("<IH", cur_data, offset=1200)
+                assert hex(self.factory) == '0x2237', 'Error mode: 0x22=VLP-16, 0x37=Strongest Return'
+                seq_index = 0
 
-            self.first_timestamp, self.factory = struct.unpack_from("<IH", cur_data, offset=1200)
-            assert hex(self.factory) == '0x2237', 'Error mode: 0x22=VLP-16, 0x37=Strongest Return'
-            seq_index = 0
+                for seq_offset in range(0, 1100, 100):
+                    self.seq_processing(cur_data, seq_offset, seq_index, self.first_timestamp, int(file_number) - 1)
 
-            for seq_offset in range(0, 1100, 100):
-                self.seq_processing(cur_data, seq_offset, seq_index, self.first_timestamp, int(file_number) - 1)
+        self.df = pd.DataFrame(self.local_df)
 
         #print('End processing pcap file...')
 
@@ -66,6 +79,8 @@ class PointReader:
         step_azimuth = 0
 
         assert hex(flag) == '0xeeff', 'Flag error'
+
+        data_dict_list = []
 
         for step in range(2):
             if step == 0 and seq_index % 2 == 0 and seq_index < 22:
@@ -86,11 +101,16 @@ class PointReader:
                 x, y, z = self.calc_real_val(arr[i * 2], azimuth, i)
                 if y > 0:
                 # azimuth_time = (55.296 / 1e6 * step + i * (2.304 / 1e6)) + first_timestamp
-                    new_row = pd.Series(
-                            [x, y, z, arr[i * 2 + 1], round(azimuth * 1.0 / self.azimuth_bin), i, first_timestamp, pcap_num],
-                            index=self.df.columns)
-                    self.local_df = self.local_df.append(new_row, ignore_index=True)
+                    d = arr[i * 2 + 1]
+                    azimuth_v = round(azimuth * 1.0 / self.azimuth_bin)
+                    data_dict_list.append({'X': x, 'Y': y, 'Z':z, 'D': d, 'azimuth': azimuth_v, 'laser_id': i, 
+                        'first_timestamp': first_timestamp, 'pcap_num': pcap_num})
+                    # new_row = pd.Series(
+                    #         [x, y, z, arr[i * 2 + 1], round(azimuth * 1.0 / self.azimuth_bin), i, first_timestamp, pcap_num],
+                    #         index=self.df.columns)
+                    # self.local_df = self.local_df.append(new_row, ignore_index=True)
             seq_index += 1
+        self.local_df += data_dict_list
 
     def calc_real_val(self, dis, azimuth, laser_id):
         r = dis * self.DISTANCE_RESOLUTION
@@ -116,7 +136,7 @@ class PointProcessing:
             self.processed_pcap = pcap_index
             index_dataframe = self.local_point_reader.read_pcap(pcap_index)
             self.full_dataframe = pd.concat([self.full_dataframe, index_dataframe], ignore_index=True)
-            print(timestamp, self.full_dataframe['first_timestamp'])
+            # print(timestamp, self.full_dataframe['first_timestamp'])
 
         if read_next_pcap_file is not False:
             print("self.processed_pcap ",self.processed_pcap)

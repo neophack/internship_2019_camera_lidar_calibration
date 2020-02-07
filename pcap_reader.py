575740925abe4ac285225523e854ca5fb9a0f6a7
Tatiana <45382631+Tatfi32@users.noreply.github.com>
2020-02-07 15:17:57 +0300

    One *pcap file in  one list version
    with comments

--- pcap_reader.py
+++ pcap_reader.py
@@ -1,87 +1,107 @@
 import struct
-from datetime import datetime
 from pathlib import Path
 import numpy as np
 import pandas as pd
 
-import time
-
-import progressbar #pip install progressbar2
-
-class Timer:    
-    def __enter__(self):
-        self.start = time.clock()
-        return self
-
-    def __exit__(self, *args):
-        self.end = time.clock()
-        self.interval = self.end - self.start
 
 class PointReader:
+
     def __init__(self, data_path):
         self.data_path = Path(data_path) / 'data'
         self.pcap_path = self.data_path / 'pcap'
-        self.pcap_files = [x for x in self.pcap_path.glob('*.*') if x.is_file()]
+        self.pcap_files =np.sort( [x for x in self.pcap_path.glob('*.*') if x.is_file()])
         self.LASER_ANGLES = [-15, 1, -13, -3, -11, 5, -9, 7, -7, 9, -5, 11, -3, 13, -1, 15]
         self.NUM_LASERS = 16
         self.DISTANCE_RESOLUTION = 0.002
         self.ROTATION_MAX_UNITS = 36000
 
-        self.df = pd.DataFrame({'X': [], 'Y': [], 'Z': [], 'D': [], 'azimuth': [], 'laser_id': [],
-                                'first_timestamp': [], 'pcap_num': []})
-        self.local_df = []
+        self.packet_df = []
+        self.seq_df = []
 
         self.azimuth_bin = 100
         self.first_timestamp = None
         self.factory = None
 
     def read_pcap(self, file_number):
-        with Timer() as t:
-            self.get_pcap_data(file_number)
-        print('PointReader.read_pcap finished in %.03f s' % t.interval)
-
-        return self.df
+        self.get_pcap_data(file_number)
+        return self.packet_df
 
     def get_pcap_data(self, file_number):
+        """
+        Args:
+            file_number: number of pcap file to be processed
 
+        Function description:
+            goes throw the all binary symbol inside pcap file
+            call seq_processing for each sequence in packets structure:
+
+            pcap file structure:
+                0 - 24   <->  global header <-> not read
+                24 - end <-> packets with 1264-binary symbol info
+
+            packets structure:
+                0 - 16      <->  local header <-> not read
+                16 - 58     <->  42 binary symbol of meta info <-> not read
+                58  - 1258  <->  12 sequences with 100 binary symbol each with dist and azimuth
+                1258 - 1264 <->  4 binary symbol for timestamp and 2 factory bites
+
+        """
         pcap_file = str(self.pcap_files[int(file_number) - 1])
         pcap_data = open(pcap_file, 'rb').read()
         pcap_data = pcap_data[24:]
 
-        with progressbar.ProgressBar(max_value=int(len(pcap_data))) as bar:
-            for offset in range(0, int(len(pcap_data)), 1264):
-                counter = offset // 1264
-                bar.update(offset)
-                # if counter % 30 == 0:
-                #    print("offset=", offset, int(len(pcap_data)))
-                    # if counter > 1:
-                    #     return
+        for offset in range(0, int(len(pcap_data)), 1264):
+            counter = offset // 1264
+            if counter % 30 == 0:
+                #print("offset=", offset, int(len(pcap_data)))
+                if self.seq_df:
+                    self.packet_df += self.seq_df
+                    self.seq_df = []
 
-                if (len(pcap_data) - offset) < 1264:
-                    break
 
-                cur_packet = pcap_data[offset + 16: offset + 16 + 42 + 1200 + 4 + 2]
-                cur_data = cur_packet[42:]
+            if (len(pcap_data) - offset) < 1264:
+                break
 
-                self.first_timestamp, self.factory = struct.unpack_from("<IH", cur_data, offset=1200)
-                assert hex(self.factory) == '0x2237', 'Error mode: 0x22=VLP-16, 0x37=Strongest Return'
-                seq_index = 0
+            cur_packet = pcap_data[offset + 16: offset + 16 + 42 + 1200 + 4 + 2]
+            cur_data = cur_packet[42:]
 
-                for seq_offset in range(0, 1100, 100):
-                    self.seq_processing(cur_data, seq_offset, seq_index, self.first_timestamp, int(file_number) - 1)
+            self.first_timestamp, self.factory = struct.unpack_from("<IH", cur_data, offset=1200)
+            assert hex(self.factory) == '0x2237', 'Error mode: 0x22=VLP-16, 0x37=Strongest Return'
+            seq_index = 0
 
-        self.df = pd.DataFrame(self.local_df)
-
-        #print('End processing pcap file...')
+            for seq_offset in range(0, 1100, 100):
+                self.seq_processing(cur_data, seq_offset, seq_index, self.first_timestamp, int(file_number) - 1)
 
     def seq_processing(self, data, seq_offset, seq_index, first_timestamp, pcap_num):
+        """
+
+        Args:
+            data: current sequence
+            seq_offset: number of the sequence in packet (0 - 1200 )
+            seq_index: number of current sub-sequence in sequence (0- 22)
+            first_timestamp: 4 binary symbol of timestamp
+            pcap_num: number of pcap file to be processed
+
+        Function description:
+        unpack sequence data into data_dict_list for each laser_id block
+        with {'X', 'Y', 'Z', 'D', 'azimuth', 'laser_id','first_timestamp', 'pcap_num'} info
+
+        data_dict_list pushed into self.seq_df
+
+
+            each sequence contain 2*11 sub-sequence
+                sub-sequence structure:
+                    flag
+                    first_azimuth
+                    16 laser_id block of binary symbol
+
+        """
         flag, first_azimuth = struct.unpack_from("<HH", data, seq_offset)
         step_azimuth = 0
 
         assert hex(flag) == '0xeeff', 'Flag error'
 
         data_dict_list = []
-
         for step in range(2):
             if step == 0 and seq_index % 2 == 0 and seq_index < 22:
                 flag, third_azimuth = struct.unpack_from("<HH", data, seq_offset + 4 + 3 * 16 * 2)
@@ -99,18 +119,15 @@ class PointReader:
                 if azimuth > self.ROTATION_MAX_UNITS:
                     azimuth -= self.ROTATION_MAX_UNITS
                 x, y, z = self.calc_real_val(arr[i * 2], azimuth, i)
-                if y > 0:
                 # azimuth_time = (55.296 / 1e6 * step + i * (2.304 / 1e6)) + first_timestamp
+                if y > 0:
                     d = arr[i * 2 + 1]
                     azimuth_v = round(azimuth * 1.0 / self.azimuth_bin)
-                    data_dict_list.append({'X': x, 'Y': y, 'Z':z, 'D': d, 'azimuth': azimuth_v, 'laser_id': i, 
-                        'first_timestamp': first_timestamp, 'pcap_num': pcap_num})
-                    # new_row = pd.Series(
-                    #         [x, y, z, arr[i * 2 + 1], round(azimuth * 1.0 / self.azimuth_bin), i, first_timestamp, pcap_num],
-                    #         index=self.df.columns)
-                    # self.local_df = self.local_df.append(new_row, ignore_index=True)
+                    data_dict_list.append({'X': x, 'Y': y, 'Z': z, 'D': d, 'azimuth': azimuth_v, 'laser_id': i,
+                                       'first_timestamp': first_timestamp, 'pcap_num': pcap_num})
+
             seq_index += 1
-        self.local_df += data_dict_list
+        self.seq_df += data_dict_list
 
     def calc_real_val(self, dis, azimuth, laser_id):
         r = dis * self.DISTANCE_RESOLUTION
@@ -119,7 +136,6 @@ class PointReader:
         x = r * np.cos(omega) * np.sin(alpha)
         y = r * np.cos(omega) * np.cos(alpha)
         z = r * np.sin(omega)
-
         return x, y, z
 
 
@@ -128,29 +144,42 @@ class PointProcessing:
         self.data_path = data_path
         self.local_point_reader = PointReader(data_path=self.data_path)
         self.processed_pcap = None
-        self.full_dataframe = pd.DataFrame()
+        self.full_dataframe = []
 
-    def get_all_points_by_timestamp(self, timestamp, pcap_index, read_next_pcap_file, azimuth=None):
+    def get_all_points_from_pcap(self, pcap_index):
+        """
+
+        Args:
+            pcap_index:
+                number of pcap file in folder (==yml VideoNumbers)
+
+        Function description:
+
+            when get new pcap_index:
+                    unpack pcap data into dataframe with self.local_point_reader.read_pcap(pcap_index)
+
+            if pcap file have already processed pcap_index:
+                return:
+                    dataframe with unpacked lidar information from 1 pcap file
+
+            Dependent functions:
+
+                self.local_point_reader.read_pcap(pcap_index)
+                return:
+                        dataframe with unpacked lidar data for one pcap file
+
+                """
 
         if pcap_index != self.processed_pcap:
             self.processed_pcap = pcap_index
-            index_dataframe = self.local_point_reader.read_pcap(pcap_index)
-            self.full_dataframe = pd.concat([self.full_dataframe, index_dataframe], ignore_index=True)
-            # print(timestamp, self.full_dataframe['first_timestamp'])
+            self.full_dataframe = self.local_point_reader.read_pcap(pcap_index)
 
-        if read_next_pcap_file is not False:
-            print("self.processed_pcap ",self.processed_pcap)
-            print('pcap_index',pcap_index)
-            pcap_number= int(pcap_index)+1
-            self.processed_pcap = "00"+str(pcap_number)
-            print("self.processed_pcap ", self.processed_pcap)
-            index_dataframe = self.local_point_reader.read_pcap(self.processed_pcap)
-            self.full_dataframe = pd.concat([self.full_dataframe, index_dataframe], ignore_index=True)
-            print(timestamp, self.full_dataframe['first_timestamp'])
 
-        if azimuth is not None:
-            return self.full_dataframe[(self.full_dataframe['azimuth'] == azimuth) &
-                                        [self.full_dataframe['first_timestamp'] == timestamp]]
         else:
-            #print(self.full_dataframe)
-            return self.full_dataframe[self.full_dataframe['first_timestamp'] == timestamp]
+            return self.full_dataframe
+
+
+
+
+
+
--- yaml_reader.py
+++ yaml_reader.py
@@ -12,50 +12,99 @@ from logger import LogWriter
 
 
 class YML_Read:
+
     def __init__(self, data_path):
+        """
+
+        Args:
+            data_path:
+            path to the Data_folder
+            Data_folder include "pcap", "yml" and "frames" folders, each with the relevant files type inside
+        """
 
         self.path = Path(data_path) / 'data'
-        self.pcap_path = self.path / 'pcap'
         self.yml_path = self.path / 'yml'
         self.images = self.path / 'frames'
 
-        self.pcap_files = np.sort([x for x in self.pcap_path.glob('*.pcap') if x.is_file()])
         self.yml_files = np.sort([x for x in self.yml_path.glob('*.gz*') if x.is_file()])
-        # cross-platform way to get file name without extention
         self.image_files = [x.stem for x in self.images.glob('*.bmp') if x.is_file()]
 
         self.processing = PointProcessing(data_path)
         self.writer = LogWriter(data_path)
 
         self.processed_images = []
-        self.VideoFlows = []
-        self.VideoNumbers = []
+        self.VideoFlows  = None
+        self.VideoNumbers = None
         self.time_lidar = None
-        self.image_number = 0
+        self.image_number = None
         self.XYZD_info = pd.DataFrame({'X': [], 'Y': [], 'Z': [], 'D': [], 'azimuth': [], 'laser_id': [],
                                        'first_timestamp': [], 'pcap_num': []})
 
     def power(self, range_yml=None):
+        """
+
+        Args:
+            range_yml:
+            number of yml file to be processed
+            if None: all files in folder
+
+        Function description:
+            goes through all yml in range_yml
+            create for each file dataframe with {'X','Y', 'Z', 'D', 'azimuth', 'laser_id', 'first_timestamp', 'pcap_num'} colums
+
+        Dependent functions:
+            self.regular_expression(yml_file.stem) - read yml file name
+            self.read_yml(yml_file path) - read each yml file data
+        """
         print('Running main loop...')
         if range_yml is None:
             range_yml = np.arange(len(self.yml_files))
         for i in range_yml:
-            self.image_number = 0
+            self.XYZD_info = pd.DataFrame({'X': [], 'Y': [], 'Z': [], 'D': [], 'azimuth': [], 'laser_id': [],
+                                           'first_timestamp': [], 'pcap_num': []})
             yml_file = self.yml_files[i]
-            file_name = yml_file.stem # cross-platform way to get file name without extention
+            file_name = yml_file.stem
 
-            self.regular_expression(yml_file=file_name)
             print(f'Reading {file_name} ...')
+            self.regular_expression(yml_file=file_name)
             self.read_yml(filename=yml_file)
 
     def regular_expression(self, yml_file):
+        """
+
+        Args:
+            yml_file:
+            name of the yml_file
+
+        Function description:
+            get current VideoNumbers and VideoFlows for the current yml file according yml file name
+
+            example:
+            new.003.001.info.yml.gz <-> yml file name
+            VideoNumbers <-> 003
+            VideoFlows <-> 001
+
+        """
         mat = re.match(r"(?P<flow>\S+)\.(?P<VideoFlow>\d+)\.(?P<VideoNumber>\d+)\.(?P<info>\S+)\.(?P<type>\d*)",
                        yml_file)
         mat = mat.groupdict()
-        self.VideoNumbers.append(mat["VideoNumber"])
-        self.VideoFlows.append(mat["VideoFlow"])
+        self.VideoNumbers = mat["VideoNumber"]
+        self.VideoFlows = mat["VideoFlow"]
 
     def read_yml(self, filename):
+        """
+
+        Args:
+            filename:
+                path to the yml file
+
+        Function description:
+            devide full yml file into shots
+            each shot processed with self.shot_processing(shot=shot)
+
+        Dependent functions:
+            self.shot_processing(shot=shot)
+        """
         with gzip.open(filename, "rt") as file:
             config = file.read()
             self.image_number = 0
@@ -67,54 +116,82 @@ class YML_Read:
                     self.shot_processing(shot=shot)
 
     def shot_processing(self, shot):
+        """
+
+        Args:
+            shot: dictionaries  with  lidar-vlp16 and camera info in one shot
+
+        Function description:
+            read key and value in shot dictionary
+
+            if key include lidar info:
+                sub-dictionary with lidar data processed with (processing.get_all_points_from_pcap)
+
+            if key include camera info:
+                each camera sub-dictionary processed with (self.camera_timestamps_processing)
+
+                if image name in "frames" folder and program already processed lidar sub-dictionary:
+
+                    saving image with self.writer.save_images in "results" folder
+                    extracting pcap data from the dataframe and saving them via self.writer.save_lidar_data
+
+        Dependent functions:
+
+        processing.get_all_points_from_pcap
+        return:
+                dataframe for current pcap data via checking yml VideoNumbers
+
+        self.camera_timestamps_processing
+        return:
+                yaml_img_name - standard form of the image name according key number
+                leftImage_deviceSec - image receiving time (Sec)
+                leftImage_grabMsec - image receiving time (Msec)
+        """
+        pacTimeStamps = None
         for key, value in sorted(shot.items(), reverse=True):
             if key.startswith("velodyneLidar"):
+
                 pacTimeStamps = shot['velodyneLidar']["lidarData"]["pacTimeStamps"]
-                self.lidar_timestamps_processing(pacTimeStamps[-1])
+                XYZD_info_temp = self.processing.get_all_points_from_pcap(pcap_index=self.VideoNumbers)
+                if XYZD_info_temp:
+                    self.XYZD_info = pd.DataFrame.from_dict(XYZD_info_temp)
 
             if key.startswith("leftImage"):
-                yaml_img_name, leftImage_deviceSec, \
-                leftImage_grabMsec = self.camera_timestamps_processing(shot['leftImage'].items())
+                yaml_img_name, leftImage_deviceSec, leftImage_grabMsec = self.camera_timestamps_processing(shot['leftImage'].items())
 
                 if yaml_img_name in self.image_files and yaml_img_name not in self.processed_images:
-
                     self.processed_images.append(yaml_img_name)
-                    self.writer.save_images(yaml_img_name=yaml_img_name,
-                                            image_time=(leftImage_grabMsec / 1e6 + leftImage_deviceSec))
-                    time_lidar = datetime.fromtimestamp(
-                        leftImage_grabMsec / 1e6 + leftImage_deviceSec).strftime('%Y-%m-%d_%H_%M_%S.%f')
-                    self.writer.save_lidar_data(time_lidar=time_lidar, df=self.XYZD_info)
 
-    def lidar_timestamps_processing(self, last_pacTimeStamp):
-        import datetime
-        start = datetime.datetime.now()
-        XYZD_info_temp = self.processing.get_all_points_by_timestamp(last_pacTimeStamp,
-                                                                     pcap_index=self.VideoNumbers[-1],
-                                                                     read_next_pcap_file = False)
-        if not XYZD_info_temp.empty:
+                    if pacTimeStamps is not None:
 
-            #print("XYZD_info_temp", XYZD_info_temp)
+                        self.writer.save_images(yaml_img_name=yaml_img_name, image_time=(leftImage_grabMsec / 1e6 + leftImage_deviceSec))
+                        time_lidar = datetime.fromtimestamp(leftImage_grabMsec / 1e6 + leftImage_deviceSec).strftime('%Y-%m-%d_%H_%M_%S.%f')
 
-            self.XYZD_info = pd.concat([self.XYZD_info, XYZD_info_temp], ignore_index=True)
-            self.XYZD_info['counter'] = self.XYZD_info.duplicated(["azimuth", "laser_id"], keep="last")
-            self.XYZD_info = self.XYZD_info[self.XYZD_info['counter'] == False]
-            self.XYZD_info = self.XYZD_info.drop(['counter'], axis=1)
-        dt = datetime.datetime.now() - start
-        #print('lidar_timestamps_processing finished in', dt, ' sec')
-
-        #print("XYZD_info ", self.XYZD_info)
+                        csv_data_timestamp = self.XYZD_info[self.XYZD_info['first_timestamp'] < int(pacTimeStamps[-1])]
+                        csv_data_timestamp['counter'] = csv_data_timestamp.duplicated(["azimuth", "laser_id"], keep="last")
+                        csv_data_timestamp = csv_data_timestamp[csv_data_timestamp['counter'] == False]
+                        self.writer.save_lidar_data(time_lidar=time_lidar, df=csv_data_timestamp)
 
     def camera_timestamps_processing(self, camera_items):
+        """
+
+        Args:
+            camera_items:
+            key from shot with camera dictionary
+
+        Returns:
+            yaml_img_name - standard form of the image name according key number
+            leftImage_deviceSec - image receiving time (Sec)
+            leftImage_grabMsec - image receiving time (Msec))
+
+        """
         leftImage_deviceSec = None
         leftImage_grabMsec = None
 
-        yaml_img_name = ("new." + str(self.VideoFlows[-1]) + '.' + str(
-            self.VideoNumbers[-1]) + '.' + 'left.' + str('%000006d' % self.image_number))
+        yaml_img_name = ("new." + str(self.VideoFlows) + '.' + str(self.VideoNumbers) + '.' + 'left.' + str('%000006d' % self.image_number))
         self.image_number += 1
 
         for key_Image, value_Image in camera_items:
-            if key_Image.startswith("deviceSec"):
-                leftImage_deviceSec = int(key_Image[len("deviceSec:"):])
-            if key_Image.startswith("grabMsec"):
-                leftImage_grabMsec = int(key_Image[len("grabMsec:"):])
+            if key_Image.startswith("deviceSec"): leftImage_deviceSec = int(key_Image[len("deviceSec:"):])
+            if key_Image.startswith("grabMsec"):  leftImage_grabMsec = int(key_Image[len("grabMsec:"):])
         return yaml_img_name, leftImage_deviceSec, leftImage_grabMsec
